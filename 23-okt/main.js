48432af88ad94da3264ecc5ae48251cb81a2d1d5
horvathmartin97 <horvathmartinn@icloud.com>
2024-12-27 20:10:06 +0100

    exercise done

--- 23-okt/main.js
+++ 23-okt/main.js
@@ -4,6 +4,37 @@ import input from './utils/input.js';
 // A vonal feletti kódrészlethez ne nyúlj, a vonal alá dolgozz!
 // ------------------------------------------------------------
 
-const userInput = await input('Így kérhetsz be adatot a felhasználótól: ');
+//const userInput = await input('Így kérhetsz be adatot a felhasználótól: ');
 
-console.log(userInput);
+//console.log(userInput);
+
+const readedData = [];
+const fileData = fs.readFileSync("data/rendel.txt", "utf-8");
+const fileDataArray = fileData.split("\r\n");
+const lane = [];
+for (let i = 0; i < fileDataArray.length; i++){
+    const row = fileDataArray[i].split(" ");
+        readedData.push({
+            orderDate:row[0],
+            cityName:row[1],
+            numberOfOrder:row[2],
+        })
+    }
+
+
+console.log(readedData);
+
+console.log("2.feladat");
+
+console.log(readedData.length);
+
+console.log("3.feladat");
+
+const userInput = await input('Kérem adjon meg egy nap számot: ');
+let orderNumber = [];
+for (let i = 0; i < readedData.length; i++){
+    if(readedData[i].orderDate === userInput){
+        orderNumber.push(readedData[i].orderDate);
+    }
+}
+console.log("Rendeleséke száma az adott napon: " + orderNumber.length);
