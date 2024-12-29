8e1ba394de4213f1d4522bc6423a2ad23ab88aa8
horvathmartin97 <horvathmartinn@icloud.com>
2024-12-29 20:14:34 +0100

    task 5 need to be fixed

--- 23-okt/main.js
+++ 23-okt/main.js
@@ -38,15 +38,14 @@ for(let i = 0; i < readedData.length; i++){
     if(readedData[i].numberOfOrder === 0){
         noAd.push(readedData[i].numberOfOrder); 
     }
-    }
-    console.log("Minden nap volt rendelés a reklámban nem éríntett városból.");
-console.log(noAd.length);
- console.log("5.feladat");
-const biggestOrders = [];
-const mostOrderDate = [];
-for(let i = 0; i  < readedData.length; i++){
-    if(readedData[i].numberOfOrder >= 9){
-        biggestOrders.push(readedData[i].numberOfOrder);
+}
+console.log("Minden nap volt rendelés a reklámban nem éríntett városból."); 
+console.log(noAd.length + " nap nem volt a reklámban nem érintett városból rendelés.");
+console.log("5.feladat");
+let biggestOrders = readedData[0].numberOfOrder;
+for (let i = 0; i < readedData.length; i++){
+    if(readedData[i].numberOfOrder > biggestOrders){
+        biggestOrders = readedData[i].numberOfOrder;
     }
 }
 console.log(biggestOrders);
