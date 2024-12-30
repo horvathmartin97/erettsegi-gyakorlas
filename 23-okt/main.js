2c72d8260f4edbb3da12af538b015d6e7fdb6fe5
horvathmartin97 <horvathmartinn@icloud.com>
2024-12-30 15:44:03 +0100

    all done

--- 23-okt/main.js
+++ 23-okt/main.js
@@ -15,9 +15,9 @@ const lane = [];
 for (let i = 0; i < fileDataArray.length; i++){
     const row = fileDataArray[i].split(" ");
         readedData.push({
-            orderDate:row[0],
+            orderDate:Number(row[0]),
             cityName:row[1],
-            numberOfOrder:row[2],
+            numberOfOrder:Number(row[2]),
         })
     }
 console.log(readedData);
@@ -35,7 +35,7 @@ console.log("Rendeleséke száma az adott napon: " + orderNumber.length);
 console.log("4.feladat");
 const noAd = [];
 for(let i = 0; i < readedData.length; i++){
-    if(readedData[i].numberOfOrder === 0){
+    if(readedData[i].numberOfOrder === 0 && readedData[i].cityName === "NR" ){
         noAd.push(readedData[i].numberOfOrder); 
     }
 }
@@ -54,3 +54,79 @@ for (let i = 0; i < readedData.length; i++){
         orderDate += readedData[i].orderDate+ " ";
 }
 console.log("A legnagyobb darabszám: " + biggestOrders, "a rendelés napja:" + orderDate);
+console.log("6.feladat");
+
+function osszes(varos,nap){
+    let everyOrder = 0;
+    for(let i = 0; i < readedData.length; i++){
+        if(readedData[i].cityName === varos && readedData[i].orderDate === nap){
+            everyOrder += readedData[i].numberOfOrder;
+        }
+    }
+    return everyOrder;
+}
+console.log("7.feladat");
+let tvAdd = 0;
+let plAdd = 0;
+let nrAdd =0;
+for (let i =0; i < readedData.length;i++){
+    if(readedData[i].orderDate === 21){
+        if(readedData[i].cityName === "TV"){
+            tvAdd += readedData[i].numberOfOrder;
+        }else if(readedData[i].cityName === "PL"){
+            plAdd += readedData[i].numberOfOrder;
+        }else if(readedData[i].cityName === "NR"){
+            nrAdd += readedData[i].numberOfOrder;
+        }
+    }
+}
+console.log(`A rendelt termékek darabszáma a 21.napon TV:${tvAdd} PL:${plAdd} NR:${nrAdd}`);
+//console.log(`A rendelt termékek darabszáma a 21. napon TV:${osszes("TV",21)} PL:${osszes("PL",21)} NR:${osszes("NR",21)}`); 
+console.log("8.feladat");
+let plArray = "PL\t";
+let tvArray = "TV\t";
+let nrArray = "NR\t";
+for(let i =0; i < 3; i++){
+    let plSum=0;
+    let tvSum=0;
+    let nrSum=0;
+    for(let j=0; j < readedData.length;j++){
+        if(i === 0){
+            if(readedData[j].orderDate < 11){
+                if(readedData[j].cityName === "PL"){
+                    plSum++;
+                }else if(readedData[j].cityName === "TV"){
+                    tvSum ++;
+                }else if( readedData[j].cityName === "NR"){
+                    nrSum ++;
+                }
+            }
+        }else if(i === 1){
+            if(readedData[j].orderDate > 10 && readedData[j].orderDate < 21){
+                if(readedData[j].cityName === "PL"){
+                    plSum++;
+                }else if(readedData[j].cityName === "TV"){
+                    tvSum ++;
+                }else if( readedData[j].cityName === "NR"){
+                    nrSum ++;
+                }
+            }
+        }else if(i === 2){
+            if(readedData[j].orderDate > 20 && readedData[j].orderDate <= 30){
+                if(readedData[j].cityName === "PL"){
+                    plSum++;
+                }else if(readedData[j].cityName === "TV"){
+                    tvSum ++;
+                }else if( readedData[j].cityName === "NR"){
+                    nrSum ++;
+                }
+               }   
+         }
+    }
+    plArray+=plSum+"\t";
+    tvArray+=tvSum+"\t";
+    nrArray+=nrSum+"\t";
+}
+let writeText = "Napok\t1..10\t11..20\t21..30";
+writeText+= "\n"+plArray+"\n"+tvArray+"\n"+nrArray;
+fs.writeFileSync("data/kampany.txt,",writeText);
